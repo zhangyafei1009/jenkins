292cfa4df0b2f1dfa93bc709c18e02a25b2bb0ad
zhangyafei <17891989109@163.com>
2023-04-26 22:52:16 +0800

    xx

---
+++ src/main/java/com/jenkins/test/JenkinsApplication.java
@@ -0,0 +1,18 @@
+package com.jenkins.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Description:
+ * @Date: 2023/04/26 22:44:35
+ * @Author: forever
+ */
+
+@SpringBootApplication
+public class JenkinsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JenkinsApplication.class, args);
+    }
+}
